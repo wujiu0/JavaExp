87a2cb3a04da83de80f1986487179a9e44dca872
wujiu0 <76419518+wujiu0@users.noreply.github.com>
2021-10-03 15:24:48 +0800

    Delete CalculatorTest.java

--- week3/exp5/CalculatorTest.java
+++
@@ -1,9 +0,0 @@
-package week3.exp5;
-
-
-public class CalculatorTest {
-    public static void main(String[] args) {
-        Calculator c = new Calculator();
-        System.out.println("计算结果为"+c.cal());
-    }
-}
