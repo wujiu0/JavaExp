87b795bcf1b59f18117c4bd384154814d2cdcee8
wujiu0 <1050705928@qq.com>
2021-10-22 21:54:39 +0800

    exp8

--- week5/exp8/content3/MainClass.java
+++ week5/exp8/content3/MainClass.java
@@ -19,14 +19,29 @@ public class MainClass {
 
         System.out.println("请输入想要查询的书名：");
         String name = sc.next();
-        Book b = bd.queryByName(name);
-        if (b != null) {
-            System.out.println(b);
+        Book book = bd.queryByName(name);
+        if (book != null) {
+            System.out.println(book);
         } else {
             System.out.println("查无此书");
         }
 
+        // 添加商品
+        System.out.println();
 
-        
+        book = new Book("b10", "新添加的书", "未知作者", 20, new Category());
+        bd.add(book);
+
+        // 修改商品
+        book = new Book("b11", "修改后的书", "未知作者", 20, new Category());
+        bd.modify(2, book);
+
+        System.out.println("添加，修改后的商品列表：");
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                System.out.println(books[i]);
+            }
+        }
+
     }
 }
